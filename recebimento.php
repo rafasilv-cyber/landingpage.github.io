016b98646cf45adad680cef9cf546ca3ed778c4f
rafasilv-cyber <rafaelgames2609@gmail.com>
2025-05-05 15:38:51 -0300

    landing

---
+++ recebimento.php
@@ -0,0 +1,10 @@
+<?php
+$name = $_POST["name"];
+echo "Cadastro realizado: $name . <br>";
+
+$email = $_POST["email"];
+echo "Cadastro realizado: $email . <br>";
+
+$data = $_POST["data nascimento"];
+echo "Cadastro realizado: $data . <br>";
+?>
